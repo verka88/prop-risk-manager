7c996eb247f8484acac0ad59f611d43d9dad568d
verka88 <zibrinovav@gmail.com>
2026-03-01 18:29:50 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,3 @@
-const CACHE_NAME = "prop-risk-manager-v1";
-
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./app.js",
-  "./manifest.json"
-];
-
-self.addEventListener("install", function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-self.addEventListener("fetch", function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        return response || fetch(event.request);
-      })
-  );
-});
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", (e) => e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch", (e) => e.respondWith(fetch(e.request)));
